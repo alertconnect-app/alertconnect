c0839455de3cbc3053b66e113986d3b8eb71bcdc
alertconnect-app <alertconnect.app@gmail.com>
2026-09-01 16:20:35 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAu91Nuo5lXUZGzarPiWjgVQlSnBR8_r30",
+  authDomain: "alertconnect-27dac.firebaseapp.com",
+  databaseURL: "https://alertconnect-27dac-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "alertconnect-27dac",
+  storageBucket: "alertconnect-27dac.firebasestorage.app",
+  messagingSenderId: "556004754007",
+  appId: "1:556004754007:web:c00286f1030afdd4c21912"
 });
 
 const messaging = firebase.messaging();
@@ -17,7 +17,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: './icon-192.png', // ഇവിടെ മാറ്റം വരുത്തിയിരിക്കുന്നു
+    icon: './icon-192.png',
     vibrate: [200, 100, 200, 100, 200, 100, 200],
     requireInteraction: true
   };
